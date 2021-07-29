575e8136250b701f351499d17909edb38698ff46
Jonathan Diaz <johndiaz693@gmail.com>
2021-07-28 20:49:09 -0700

    Update discordBotQuery.sql

--- discordBotQuery.sql
+++ discordBotQuery.sql
@@ -2,13 +2,6 @@ DROP DATABASE Noodle;
 CREATE DATABASE JonathanDiscordBot;
 USE JonathanDiscordBot;
 
-/*
-CREATE TABLE NoodleTable ( 
-id VARCHAR(30), 
-xp INT, 
-cur BIGINT
-);
-*/
 
 CREATE TABLE BotTable ( 
 id VARCHAR(30), 
